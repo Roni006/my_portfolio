1ad4de7ceb92c4abf7a64f55bce29a0cf49ffb56
Anik Islam <anik54908@gmail.com>
2024-06-05 11:48:43 +0600

    create hero-section half

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideMenu(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'none'
+}
